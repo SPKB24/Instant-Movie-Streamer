3e1dc03026725317046df60a8a86c35d688e19ae
Sohit Pal <sohitpal@outlook.com>
2017-10-17 01:33:14 -0400

    Initial Commit: Going to add YTS.ag support

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 from setuptools import setup
 
 setup(name="ims",
-	  version="3.3",
-	  description="Instantly stream movies/ tv episodes",
-	  url="http://iyask.me",
-	  author="Yask Srivastava",
-	  author_email="yask123@gmail.com",
-	  license='MIT',
-	  packages=["ims"],
-	  scripts=["bin/ims"],
-	  install_requires=[
-		  'BeautifulSoup4',
-		  'requests'],
-	  zip_safe=False)
+        version="3.3",
+           description="Instantly stream movies/ tv episodes",
+        url="http://iyask.me",
+    author="Yask Srivastava",
+         author_email="yask123@gmail.com",
+        license='MIT',
+        packages=["ims"],
+        scripts=["bin/ims"],
+        install_requires=[
+            'BeautifulSoup4',
+            'requests'],
+        zip_safe=False)
