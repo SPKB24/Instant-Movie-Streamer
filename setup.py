daf660612005edb564f760b09ab6b5e7319d5e5d
Sohit Pal <sohitpal@outlook.com>
2017-10-19 11:57:34 -0400

    Fix formatting - minor

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(name="ims",
         version="3.3",
-           description="Instantly stream movies/ tv episodes",
+        description="Instantly stream movies/ tv episodes",
         url="http://iyask.me",
-    author="Yask Srivastava",
-         author_email="yask123@gmail.com",
+        author="Yask Srivastava",
+        author_email="yask123@gmail.com",
         license='MIT',
         packages=["ims"],
         scripts=["bin/ims"],
